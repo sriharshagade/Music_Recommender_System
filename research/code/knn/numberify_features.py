60583155addf9916e595ef3daa2421c4800c3433
Sriharsha Gade <gsriharsha98@gmail.com>
2021-06-15 22:08:02 +0530

    Add files via upload

---
+++ research/code/knn/numberify_features.py
@@ -0,0 +1,21 @@
+# Script to convert evaluation triples to numeric form
+# Usage: python numberify.py <userfile> <songfile> <infile> <outfile>
+
+import sys
+
+userfile, songfile, infile, outfile = sys.argv[1:]
+
+users = {}
+for n, line in enumerate(open(userfile), start=1):
+  users[line.strip()] = n
+
+songs = {}
+for n, line in enumerate(open(songfile), start=1):
+  songs[line.strip()] = n
+
+with open(outfile, 'wb') as out:
+  out.write('user,song,count\n')
+  for line in open(infile):
+    user, song, count = line.split()
+    out.write('%d,%s,%s\n' % (users[user], songs[song], count))
+
